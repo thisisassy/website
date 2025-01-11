ef4fe938ff441b000a9fe4f335a82d6faded72fe
Haruka Ashiuchi <chopperman1224@gmail.com>
2025-01-11 18:03:03 +0900

    他のページを作る

---
+++ app/blog/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./blog.module.scss";
+
+export default function Blog() {
+  return (
+    <div className={styles.blog__content}>
+      <h2 className={styles.heading}>Blog</h2>
+    </div>
+  );
+}
---
+++ app/works/page.tsx
@@ -0,0 +1,10 @@
+import styles from "./works.module.scss";
+
+export default function Works() {
+  return (
+    <div className={styles.works__content}>
+      <h2 className={styles.heading}>Works</h2>
+      <p>coming soon</p>
+    </div>
+  );
+}
